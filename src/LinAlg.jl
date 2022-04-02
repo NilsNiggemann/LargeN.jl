d0845c2194931a1280235474884b88c59743d6cd
Nils Niggemann <niggeni@zedat.fu-berlin.de>
2022-04-02 21:22:28 +0530

    performance tweaks for eigvals and better analysis fallback

--- src/LinAlg.jl
+++ src/LinAlg.jl
@@ -1,6 +1,6 @@
-function M_inverse(β::Real,J̃::AbstractMatrix,λ::Real)
+function M_inverse(T::Real,J̃::AbstractMatrix,λ::Real)
     dims = size(J̃)
-    inv(β .* J̃+λ .* Matrix(I,dims...))
+    inv(J̃ ./T +λ .* Matrix(I,dims...))
 end
 
 function X_q(nb::Integer,Minv::AbstractMatrix)
@@ -18,8 +18,8 @@ end
 getNCell(JMat::AbstractMatrix) = size(JMat)[1]
 
 
-function X_q(β::Real,J̃::AbstractMatrix,λ::Real,nb::Integer = getNCell(J̃))
-    real(sum(M_inverse(β,J̃,λ)))/nb
+function X_q(T::Real,J̃::AbstractMatrix,λ::Real,nb::Integer = getNCell(J̃))
+    real(sum(M_inverse(T,J̃,λ)))/nb
 end
 
 function constraint(J̃::Function,λ::Real,T::Real,BZextent::Real,d::Integer = getDimfromFunc(J̃);kwargs...)
@@ -43,8 +43,8 @@ function ComputeEigvals2D(Jfunc::Function,nk::Integer,ext;min = -ext,max = ext)
     NCell = getNCell(Jfunc)
     eig = Array{Float64}(undef,NCell,nk,nk)
     for (i,kx) in enumerate(karray), (j,ky) in enumerate(karray)
-        sys = eigen(Jfunc(SA[kx,ky]))
-        eig[:,i,j] .= sys.values
+        EV = eigvals(Jfunc(SA[kx,ky]))
+        eig[:,i,j] .= EV
     end
     return eig
 end
@@ -54,8 +54,8 @@ function ComputeEigvals3D(Jfunc::Function,nk::Integer,ext;min = -ext,max = ext)
     NCell = getNCell(Jfunc)
     eig = Array{Float64}(undef,NCell,nk,nk,nk)
     for (i,kx) in enumerate(karray), (j,ky) in enumerate(karray),(k,kz) in enumerate(karray)
-        sys = eigen(Jfunc(SA[kx,ky,kz]))
-        eig[:,i,j,k] .= sys.values
+        EV = eigvals(Jfunc(SA[kx,ky,kz]))
+        eig[:,i,j,k] .= EV
     end
     return eig
 end
@@ -80,11 +80,11 @@ function getEvals(JFunc,BZextent = 4pi;nk = 20)
     EV= evalFunc(JFunc,nk,BZextent)
 end
 
-function getChiFunction(T,Sys::Geometry,Mod::Module;BZextent = 4pi,nk = 20,tol = 1e-6,verbose = true)
+function analyzeSpectrum(T,Sys::Geometry,Mod::Module;BZextent = 4pi,nk = 20,verbose = true)
     JFunc = constructJ(Sys,Mod)
     EV = getEvals(JFunc,BZextent,nk = nk)
+
     Emin = minimum(EV)
-    
     degeneracy = length(filter(x -> isapprox(x,Emin,atol = 1e-8),EV))#/length(EV)
 
     NCell = getNCell(JFunc)
@@ -92,27 +92,37 @@ function getChiFunction(T,Sys::Geometry,Mod::Module;BZextent = 4pi,nk = 20,tol =
 
     LamSing = -Emin/T
     verbose && println("-Emin/T: $LamSing")
-    beta = 1 /T
 
-    Lam = LamSing
+    LeftBound = LamSing
+    RightBound = LamSing+10+T
+    return (JFunc = JFunc, EV = EV, LamSing = LamSing, LeftBound = LeftBound, RightBound = RightBound,constraint = x->constraint(EV,x,T))
+end
+
+function getChiFunction(T,Sys::Geometry,Mod::Module;BZextent = 4pi,nk = 20,tol = 1e-6,verbose = true)
+    sinfo = analyzeSpectrum(T,Sys,Mod;BZextent = BZextent,nk = nk,verbose = verbose)
+
+    JFunc = sinfo[:JFunc]
+    EV = sinfo[:EV]
+    Lam = sinfo[:LamSing]
+    LeftBound = sinfo[:LeftBound]
+    RightBound = sinfo[:RightBound]
+
     cons = 1E16
     try
-        Lam = optimizeConstraint(EV,T,guess=[LamSing,LamSing+10+T])
+        Lam = optimizeConstraint(EV,T,guess=[LeftBound,RightBound])
         # Lam = optimizeConstraint(JFunc,T,BZextent,guess=[LamSing+0.1*tol,LamSing+10+T])
         cons = constraint(EV,Lam,T)
         verbose && @info "constraint minimized to $cons for λ = $Lam"
     catch e
         verbose && @warn "constraint could not be optimized!
-        (Errored with: $e) Lambda set to singularity"
+        (Errored with: $e). Lam set to singularity."
     end
 
     if abs(cons) > tol
         @warn "constraint possibly not converged! (constraint tolerance set to $tol)"
     end
-    println(NCell + LamSing-Lam)
 
-
-    @inline chi(q)= X_q(beta,JFunc(q),Lam)
+    @inline chi(q)= X_q(T,JFunc(q),Lam)
 
     return chi
 end
