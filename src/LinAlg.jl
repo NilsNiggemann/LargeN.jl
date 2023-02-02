b8befd381e7a57f5ca6d771dd596f2ed551aa3eb
Nils Niggemann <nils.niggemann@fu-berlin.de>
2023-02-02 15:17:46 +0100

    performance improvements

--- src/LinAlg.jl
+++ src/LinAlg.jl
@@ -1,10 +1,5 @@
 function M_inverse(T::Real,J̃::AbstractMatrix,λ::Real)
-    dims = size(J̃)
-    inv(J̃ ./T +λ .* Matrix(I,dims...))
-end
-
-function X_q(nb::Integer,Minv::AbstractMatrix)
-    real(sum(Minv)/nb)
+    inv(J̃ ./T +λ *I)
 end
 
 function getNCell(Jfunc::Function)
@@ -18,13 +13,13 @@ end
 getNCell(JMat::AbstractMatrix) = size(JMat)[1]
 
 
-function X_q(T::Real,J̃::AbstractMatrix,λ::Real,nb::Integer = getNCell(J̃))
-    real(sum(M_inverse(T,J̃,λ)))/nb
+function X_q(T::Real,J̃::AbstractMatrix,λ::Real)
+    real(sum(M_inverse(T,J̃,λ)))
 end
 
 function constraint_cont(J̃::Function,λ::Real,T::Real,BZextent::Real,d::Integer = getDimfromFunc(J̃);kwargs...)
     f(q) = real(tr(M_inverse(T,J̃(q),λ)))
-    return 1/(BZextent)^d * BZIntegral(f,d,BZextent;kwargs...) -1
+    return 1/(BZextent)^d * BZIntegral(f,d,BZextent;kwargs...) -1/3
 end
 
 function optimizeConstraint(J̃::Function,T::Real,BZextent::Real,d::Integer= getDimfromFunc(J̃);min = 2/T+0.01, max=2/T+10,guess = (min,max),method = Roots.Bisection(),kwargs... )
@@ -37,13 +32,13 @@ function BZIntegral(f::Function,d::Integer,BZextent::Real; kwargs...)
     return val
 end
 
-function X_q_eig(T::Real,J̃::AbstractMatrix,λ::Real,nb::Integer = getNCell(J̃))
+function X_q_eig(T::Real,J̃::AbstractMatrix,λ::Real)
     evals, evecs = eigen(J̃)
     sum = 0. +0im
     for alpha in eachindex(evals),beta in eachindex(evals),gamma in eachindex(evals)
         sum += (evecs[alpha,beta] *conj(evecs[gamma,beta] ))/(evals[beta]/T + λ)
     end
-    return real(sum/nb)
+    return real(sum)
 end
 
 function zeroTweight(J̃::AbstractMatrix)
@@ -90,9 +85,16 @@ function ComputeEigvals2D(Jfunc::Function,nk::Integer,ext;min = -ext,max = ext)
     karray = range(min,max,length = nk)
     NCell = getNCell(Jfunc)
     eig = Array{T}(undef,NCell,nk,nk)
-    for (i,kx) in enumerate(karray), (j,ky) in enumerate(karray)
-        EV =  eigvals(Jfunc(SA[kx,ky]))
-        eig[:,i,j] .= EV
+
+    Threads.@threads for i in 1:nk
+        JArr = Array{Complex{T}}(undef,NCell,NCell) # eigvals! allocates much more anyway so this is fine
+        kx = karray[i]
+        for j in 1:nk
+            ky = karray[j]
+            JArr .= Jfunc(SA[kx,ky])
+            EV = eigvals!(JArr)
+            eig[:,i,j] .= EV
+        end
     end
     return eig
 end
@@ -102,9 +104,19 @@ function ComputeEigvals3D(Jfunc::Function,nk::Integer,ext;min = -ext,max = ext)
     karray = range(min,max,length = nk)
     NCell = getNCell(Jfunc)
     eig = Array{T}(undef,NCell,nk,nk,nk)
-    for (i,kx) in enumerate(karray), (j,ky) in enumerate(karray),(k,kz) in enumerate(karray)
-        EV = eigvals(Jfunc(SA[kx,ky,kz]))
-        eig[:,i,j,k] .= EV
+
+    Threads.@threads for i in 1:nk
+        JArr = Array{Complex{T}}(undef,NCell,NCell) # eigvals! allocates much more anyway so this is fine
+        kx = karray[i]
+        for j in 1:nk
+            ky = karray[j]
+            for k in 1:nk
+                kz = karray[k]
+                JArr .= Jfunc(SA[kx,ky,kz])
+                EV = eigvals!(JArr)
+                eig[:,i,j,k] .= EV
+            end
+        end
     end
     return eig
 end
@@ -112,10 +124,11 @@ end
 
 @inline function constraint(eval::AbstractArray,Lam::Real,T::FloatType) where FloatType
     s = zero(FloatType)
+    LamT = Lam*T
     for e in eval
-        s += 1/(e/T+Lam)
+        s += 1/(e+LamT)
     end
-    s/length(eval) - one(FloatType)
+    T*s/length(eval) - 1/3*one(FloatType)
 end
 
 function optimizeConstraint(eval::AbstractArray,T::Real;guess =3/T,kwargs...)
@@ -174,9 +187,11 @@ function getChiFunction(T,Sys::Geometry,Basis::Basis_Struct,pairToInequiv::Funct
         @warn "T= $T: constraint possibly not converged!
         constraint value: $cons \t tolerance: $tol"
     end
+    
 
     @inline chi(q::AbstractVector)= X_q(T,JFunc(q),Lam)
-    @inline chi(q...)= X_q(T,JFunc(SVector(q)),Lam)
+    @inline chi(qx::Real,qy::Real)= X_q(T,JFunc(SA[qx,qy]),Lam)
+    @inline chi(qx::Real,qy::Real,qz::Real)= X_q(T,JFunc(SA[qx,qy,qz]),Lam)
 
     return chi
 end
